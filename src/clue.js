0baf142f89be7d9a86adf1e89a1a5a7ecf9aac18
Michael Heimrath <mheimrath1994@gmail.com>
2021-01-13 14:39:27 +0100

    done

--- src/clue.js
+++ src/clue.js
@@ -1,14 +1,168 @@
 // ITERATION 1
 
 // Suspects Collection
-const suspectsArray = [];
+const suspectsArray = [
+    {
+        firstName: '',
+        lastName: '',
+        occupation: '',
+        age: '',
+        description: '',
+        image: '',
+        color: ''
+    },
+    {
+        firstName: '',
+        lastName: '',
+        occupation: '',
+        age: '',
+        description: '',
+        image: '',
+        color: ''
+    },
+    {
+        firstName: '',
+        lastName: '',
+        occupation: '',
+        age: '',
+        description: '',
+        image: '',
+        color: ''
+    },
+    {
+        firstName: '',
+        lastName: '',
+        occupation: '',
+        age: '',
+        description: '',
+        image: '',
+        color: ''
+    },
+    {
+        firstName: '',
+        lastName: '',
+        occupation: '',
+        age: '',
+        description: '',
+        image: '',
+        color: ''
+    },
+    {
+        firstName: '',
+        lastName: '',
+        occupation: '',
+        age: '',
+        description: '',
+        image: '',
+        color: ''
+    },
+];
 
 // Rooms Collection
-const roomsArray = [];
+const roomsArray = [
+    {
+        name: '1'
+    },
+    {
+        name: '2'
+    },
+    {
+        name: '3'
+    },
+    {
+        name: '4'
+    },
+    {
+        name: '5'
+    },
+    {
+        name: '6'
+    },
+    {
+        name: '7'
+    },
+    {
+        name: '8'
+    },
+    {
+        name: '9'
+    },
+    {
+        name: '10'
+    },
+    {
+        name: '11'
+    },
+    {
+        name: '12'
+    },
+    {
+        name: '13'
+    },
+    {
+        name: '14'
+    },
+    {
+        name: '15'
+    },
+    
+];
 
 // Weapons Collection
-const weaponsArray = [];
+const weaponsArray = [
+    {
+        name: '',
+        weight: ''
+    },
+    {
+        name: '',
+        weight: ''
+    },
+    {
+        name: '',
+        weight: ''
+    },
+    {
+        name: '',
+        weight: ''
+    },
+    {
+        name: '',
+        weight: ''
+    },
+    {
+        name: '',
+        weight: ''
+    },
+    {
+        name: '',
+        weight: ''
+    },
+    {
+        name: '',
+        weight: ''
+    },
+    {
+        name: '',
+        weight: ''
+    }
+];
 
 // ITERATION 2
+function selectRandom(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
 
+function pickMystery() {
+    return {
+        suspect: selectRandom(suspectsArray),
+        weapon: selectRandom(weaponsArray),
+        room: selectRandom(roomsArray)
+    }
+}
+let envelope = pickMystery();
 // ITERATION 3
+function revealMystery(object) {
+   return (`${object.suspect.firstName} ${object.suspect.lastName} killed Mr. Boddy using the ${object.weapon.name} in the ${object.room.name}!`);
+}
+revealMystery(envelope)
